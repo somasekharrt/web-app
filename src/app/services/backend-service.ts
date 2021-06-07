c322cc9fc60943b8798214e9bc8a0caa96298791
Naseema <naseemabeesingapore@gmail.com>
2021-06-07 11:27:02 +0530

    localhost to cluster ip

--- src/app/services/backend-service.ts
+++ src/app/services/backend-service.ts
@@ -12,6 +12,6 @@ export class BackendServiceComponent {
 
    constructor( private http: HttpClient) {}
   getDataFromBackend(): Observable<any> {
-  return this.http.get('http://localhost:9003/backendCall');
+  return this.http.get('http://172.18.0.1:9003/backendCall');
 }
 }
